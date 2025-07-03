73835398a346e421fe3b345afa9b1f9914ad8b94
Jason Stanley <dummy.jason00@gmail.com>
2025-07-03 07:20:39 +0700

    add dna when mint

--- src/impls.rs
+++ src/impls.rs
@@ -2,10 +2,10 @@ use super::*;
 use frame::prelude::*;
 
 impl<T: Config> Pallet<T> {
-	pub fn mint(owner: T::AccountId) -> DispatchResult {
+	pub fn mint(owner: T::AccountId, dna: [u8; 32]) -> DispatchResult {
 		let current_count = CountForKitten::<T>::get();
 		let new_count = current_count.checked_add(1).ok_or(Error::<T>::TooManyKitties)?;
-
+		Kitties::<T>::insert(dna, ());
 		CountForKitten::<T>::set(new_count);
 		Self::deposit_event(Event::<T>::Created { owner });
 		Ok(())
